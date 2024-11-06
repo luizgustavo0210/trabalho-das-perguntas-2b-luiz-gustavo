0af10348e8b276f526fa9b2238288bc83587a848
luizgustavo0210 <eletrica2019anderson@gmail.com>
2024-11-06 11:00:11 -0300

    projeto alura 2 ano B

---
+++ luiz_projetoalura/perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'historia',
+    'quem foi antonieta de Barros ?',
+    'antonieta de Barros é uma das catarinenses mais importantes de todos os tempos. Foi a primeira negra a ser eleita deputada estadual no país.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'português',
+    'oque é uma estrofe?',
+    'é cada um dos “bloquinhos” que compõem o poema.'
+)
+
+criaCartao(
+    'musica',
+    'Como é uma semínima?',
+    'É uma nota musical'
+)
+
